6c941e35cd2178797df76f389d3dfccb2968beda
karis courey <kariscourey@gmail.com>
2023-01-11 22:12:36 -0500

    fix view color invert

--- tokyo/src/components/Recommendations.jsx
+++ tokyo/src/components/Recommendations.jsx
@@ -9,15 +9,13 @@ const RecLinks = [
 const Recommendations = () => {
   return (
     <>
-
-            <a href={`${RecLinks[0].link}`} target="_blank" rel="noreferrer">
-              <img
-                className="svg"
-                src={`assets/img/svg/social/${RecLinks[0].iconName}.svg`}
-                alt="social"
-              ></img>
-            </a>
-
+      <a href={`${RecLinks[0].link}`} target="_blank" rel="noreferrer">
+        <img
+          className="svg"
+          src={`assets/img/svg/social/${RecLinks[0].iconName}.svg`}
+          alt="social"
+        ></img>
+      </a>
       {/* END RECOMMENDATIONS */}
     </>
   );
