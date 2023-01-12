37c4aa09e7e611c49aef4f93a947ada5d4be6350
karis courey <kariscourey@gmail.com>
2023-01-11 20:32:12 -0500

    enable SPA

--- tokyo/src/router/Routes.js
+++ tokyo/src/router/Routes.js
@@ -3,10 +3,13 @@ import HomeLight from "../views/all-home-version/HomeLight";
 import NotFound from "../views/NotFound";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
+const domain = /https:\/\/[^/]+/;
+const basename = process.env.PUBLIC_URL.replace(domain, '');
+
 const Routes = () => {
   return (
     <>
-      <Router>
+      <Router basename={basename}>
         <Switch>
           <Route exact path="/" component={HomeLight} />
           <Route component={NotFound} />
